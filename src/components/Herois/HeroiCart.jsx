0f4032e75f87c67dfed25f4871d9e52296aa21a6
Ehorga <yehornahiba1207@gmail.com>
2026-01-14 16:58:23 +0200

    addRatiing

--- src/components/Herois/HeroiCart.jsx
+++ src/components/Herois/HeroiCart.jsx
@@ -1,18 +1,35 @@
 import PropTypes from "prop-types";
-import styles from './Herois.module.css'
+import styles from "./Herois.module.css";
 import classNames from "classnames";
 const HeroiCart = (props) => {
-  const { hero , setSelectHero, isSelect} = props;
-  const choiceHero =()=>{
-    setSelectHero(hero)
-  }
-const heroClass = classNames(styles.article, {
-    [styles["select"]]:isSelect
-})
+  const { hero, setSelectHero, isSelect, changHeroRating } = props;
+  const choiceHero = () => {
+    setSelectHero(hero);
+  };
+  const heroClass = classNames(styles.article, {
+    [styles["select"]]: isSelect,
+  });
+  const addRating = (event) => {
+    event.stopPropagation();
+    changHeroRating(hero.id, Math.min(5, hero.rating + 1));
+    // changHeroRating(hero.id, hero.rating + 1 >= 5? 5 : hero.rating +1);
+  };
+  const subRating = (event) => {
+    event.stopPropagation();
+    changHeroRating(hero.id, Math.max(1, hero.rating - 1));
+    // changHeroRating(hero.id, hero.rating - 1 <= 1? 1 : hero.rating -1);
+  };
   return (
     <article className={heroClass} onClick={choiceHero}>
       <h3>{hero.name}</h3>
-      <p>power : {hero.power}</p>
+      <div className={styles.info}>
+        <p>power : {hero.power}</p>
+        <div>
+          <span>{hero.rating}</span>
+          <button onClick={addRating}>👍🏻</button>
+          <button onClick={subRating}>👎🏻</button>
+        </div>
+      </div>
     </article>
   );
 };
--- src/components/Herois/Herois.jsx
+++ src/components/Herois/Herois.jsx
@@ -5,23 +5,35 @@ import { useState } from "react";
 
 const Herois = () => {
   const [selectHero, setSelectHero] = useState("");
+  const [listHerois, setListHerois] = useState(herois);
+  const changHeroRating = (id, newRating) => {
+    setListHerois((prev) =>
+      prev.map((hero) =>
+        hero.id === id ? { ...hero, rating: newRating } : hero
+      )
+    );
+    if (selectHero?.id === id ) {
+      setSelectHero((prev)=>({...prev,rating:newRating}))
+    }
+  };
   const ShowHeroCart = (hero) => (
     <HeroiCart
       key={hero.id}
       hero={hero}
       setSelectHero={setSelectHero}
       isSelect={selectHero?.id === hero.id}
+      changHeroRating={changHeroRating}
     />
   );
   return (
     <section>
       <h2 className={styles.title}>Choose favorite hero</h2>
 
-      <div className={styles.herois}>{herois.map(ShowHeroCart)}</div>
+      <div className={styles.herois}>{listHerois.map(ShowHeroCart)}</div>
       <h2 className={styles.title}>Your choice</h2>
       <div className={styles.selectHero}>
         {selectHero ? (
-          <HeroiCart hero={selectHero} />
+          <HeroiCart hero={selectHero} changHeroRating={changHeroRating} />
         ) : (
           <p>Героя ще не вибрано</p>
         )}
