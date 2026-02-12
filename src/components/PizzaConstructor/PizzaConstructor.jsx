f677b468583da72f4849afc3209e2db3c72402d0
Ehorga <yehornahiba1207@gmail.com>
2026-02-12 17:35:20 +0200

    pizzaConstructorJSX

--- src/components/PizzaConstructor/PizzaConstructor.jsx
+++ src/components/PizzaConstructor/PizzaConstructor.jsx
@@ -11,29 +11,73 @@ const PizzaConstructor = () => {
   const [topping, setTopping] = useState([]);
   const [dough, setDough] = useState(DOUGH_TYPES[0]);
   const [amount, setAmount] = useState(1);
-  const [order, setOrder] = useState({ size, dough , topping , amount , total:0});
-
-  const showSize = (size) => (
-    <label key={size.label}>
-      <input type="radio" value={size.value} checked={size=== size.value} onChange={()=>setSize(size.value)} name="size" />
-      <span>{size.label}</span>
+  const [isOrder, setIsOrder] = useState(false);
+  const calculate = () => {
+    const toppingPrice = topping.length * TOPPING_PRICE;
+    const sizePrice =
+      BASE_PRICE * PIZZA_SIZES.find((s) => s.value === size).multiplier;
+    return (toppingPrice + sizePrice) * amount;
+  };
+  const order = { size, dough, topping, amount, total: calculate() };
+  const showSize = (s) => (
+    <label key={s.label}>
+      <input
+        type="radio"
+        value={s.value}
+        checked={size === s.value}
+        onChange={() => {
+          setSize(s.value);
+        }}
+        name="size"
+      />
+      <span>{s.label}</span>
     </label>
   );
-  const showTopping = (topping) => (
-    <label key={topping}>
-      <input type="checkbox" value={topping} name="topping" />
-      <span>{topping}</span>
+  const showTopping = (t) => (
+    <label key={t}>
+      <input
+        type="checkbox"
+        value={t}
+        checked={topping.includes(t)}
+        onChange={() => {
+          changeTopping(t);
+        }}
+        name="topping"
+      />
+      <span>{t}</span>
     </label>
   );
-  const showDough = (dough) => (
-    <option value={dough} key={dough}>
-      {dough}{" "}
+  const showDough = (d) => (
+    <option value={d} key={d}>
+      {d}{" "}
     </option>
   );
+  const changeTopping = (t) => {
+    setTopping((prev) =>
+      prev.includes(t) ? prev.filter((tp) => tp !== t) : [...prev, t],
+    );
+  };
+  if (isOrder) {
+    return (
+      <article>
+        <h2>Дякуємо за замовлення</h2>
+        <p>Очікуйте доставку піци.</p>
+        <p>
+          Розмір: {order.size}cm. Тип тіста: {order.dough}. Топінги: {order.topping.length>0?order.topping.join(", "):"none"}. Кількість порцій: {order.amount}.
+        </p>
+        <p>До оплати кур'єру: {order.total} грн</p>
+      </article>
+    );
+  }
   return (
     <section>
       <h2>Конструктор піци</h2>
-      <form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          setIsOrder(true);
+        }}
+      >
         <fieldset>
           <legend>Розмір піци</legend>
           <div>{PIZZA_SIZES.map(showSize)}</div>
@@ -44,11 +88,22 @@ const PizzaConstructor = () => {
         </fieldset>
         <fieldset>
           <legend>Тип тіста</legend>
-          <select> {DOUGH_TYPES.map(showDough)}</select>
+          <select
+            value={dough}
+            onChange={(event) => setDough(event.target.value)}
+          >
+            {" "}
+            {DOUGH_TYPES.map(showDough)}
+          </select>
         </fieldset>
         <fieldset>
           <legend>Кількість порцій</legend>
-          <input type="number" min={1} />
+          <input
+            type="number"
+            value={amount}
+            onChange={(event) => setAmount(event.target.value)}
+            min={1}
+          />
         </fieldset>
         <button type="submit">Замовити</button>
       </form>
@@ -64,7 +119,7 @@ const PizzaConstructor = () => {
         </p>
         <p>
           <strong>Топінги:</strong>
-          {order.topping.length>0? order.topping.join(", ") : "немає"}
+          {order.topping.length > 0 ? order.topping.join(", ") : "немає"}
         </p>
         <p>
           <strong>Кількість порцій:</strong>
