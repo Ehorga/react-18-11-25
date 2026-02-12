088d915084bb7e8277bc1fe3458e594474f84806
Ehorga <yehornahiba1207@gmail.com>
2026-02-12 16:55:02 +0200

    bugCheck

--- src/components/PizzaConstructor/PizzaConstructor.jsx
+++ src/components/PizzaConstructor/PizzaConstructor.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   PIZZA_SIZES,
   TOPPINGS,
@@ -6,9 +7,15 @@ import {
   TOPPING_PRICE,
 } from "./data";
 const PizzaConstructor = () => {
+  const [size, setSize] = useState(PIZZA_SIZES[0].value);
+  const [topping, setTopping] = useState([]);
+  const [dough, setDough] = useState(DOUGH_TYPES[0]);
+  const [amount, setAmount] = useState(1);
+  const [order, setOrder] = useState({ size, dough , topping , amount , total:0});
+
   const showSize = (size) => (
     <label key={size.label}>
-      <input type="radio" value={size.value} name="size" />
+      <input type="radio" value={size.value} checked={size=== size.value} onChange={()=>setSize(size.value)} name="size" />
       <span>{size.label}</span>
     </label>
   );
@@ -45,6 +52,29 @@ const PizzaConstructor = () => {
         </fieldset>
         <button type="submit">Замовити</button>
       </form>
+      <article>
+        <h2>Ваше замовлення:</h2>
+        <p>
+          <strong>Розмір:</strong>
+          {order.size}
+        </p>
+        <p>
+          <strong>Тип тіста:</strong>
+          {order.dough}
+        </p>
+        <p>
+          <strong>Топінги:</strong>
+          {order.topping.length>0? order.topping.join(", ") : "немає"}
+        </p>
+        <p>
+          <strong>Кількість порцій:</strong>
+          {order.amount}
+        </p>
+        <p>
+          <strong>Сума:</strong>
+          {order.total}
+        </p>
+      </article>
     </section>
   );
 };
